63940e4eb01e3ca1806e013b726d72e266c67524
nguyenthanhdat2005tm2-eng <nguyenthanhdat2005.tm2@gmail.com>
2026-07-04 11:01:59 +0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,8 +12,7 @@ public_users.post("/register", (req,res) => {
   if (username && password) {
     if (!isValid(username)) {
       users.push({"username":username,"password":password});
-      // Giữ nguyên lỗi chính tả 'registred' của IBM để máy chấm chịu
-      return res.status(200).json({message: "User successfully registred. Now you can login"});
+      return res.status(200).json({message: "User successfully registered. Now you can login"});
     } else {
       return res.status(404).json({message: "User already exists!"});
     }
@@ -21,71 +20,67 @@ public_users.post("/register", (req,res) => {
   return res.status(404).json({message: "Unable to register user."});
 });
 
-// Task 1: Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.status(200).send(JSON.stringify(books, null, 4));
+// Task 10: Get the book list available in the shop using Promises
+public_users.get('/', function (req, res) {
+  new Promise((resolve, reject) => {
+      resolve(books);
+  })
+  .then(bookList => res.status(200).send(JSON.stringify(bookList, null, 4)))
+  .catch(error => res.status(500).json({message: "Error fetching books"}));
 });
 
-// Task 2: Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+// Task 11: Get book details based on ISBN using Promises
+public_users.get('/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
-  return res.status(200).send(books[isbn]);
+  new Promise((resolve, reject) => {
+      if (books[isbn]) {
+          resolve(books[isbn]);
+      } else {
+          reject({status: 404, message: "ISBN not found"});
+      }
+  })
+  .then(book => res.status(200).send(book))
+  .catch(error => res.status(error.status).json({message: error.message}));
 });
   
-// Task 3: Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+// Task 12: Get book details based on author using Promises
+public_users.get('/author/:author', function (req, res) {
   const author = req.params.author;
-  let result = Object.values(books).filter(book => book.author === author);
-  return res.status(200).send(result);
+  new Promise((resolve, reject) => {
+      let result = Object.values(books).filter(book => book.author === author);
+      if (result.length > 0) {
+          resolve(result);
+      } else {
+          reject({status: 404, message: "Author not found"});
+      }
+  })
+  .then(result => res.status(200).send(result))
+  .catch(error => res.status(error.status).json({message: error.message}));
 });
 
-// Task 4: Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+// Task 13: Get all books based on title using Promises
+public_users.get('/title/:title', function (req, res) {
   const title = req.params.title;
-  let result = Object.values(books).filter(book => book.title === title);
-  return res.status(200).send(result);
+  new Promise((resolve, reject) => {
+      let result = Object.values(books).filter(book => book.title === title);
+      if (result.length > 0) {
+          resolve(result);
+      } else {
+          reject({status: 404, message: "Title not found"});
+      }
+  })
+  .then(result => res.status(200).send(result))
+  .catch(error => res.status(error.status).json({message: error.message}));
 });
 
-// Task 5: Get book review
+//  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   const isbn = req.params.isbn;
-  return res.status(200).send(books[isbn].reviews);
+  if(books[isbn]) {
+      res.status(200).send(books[isbn].reviews);
+  } else {
+      res.status(404).send("Book not found");
+  }
 });
-
-// --- TASK 11: MỒI CHO MÁY CHẤM ĐIỂM (AXIOS & PROMISES) ---
-
-// 1. Lấy danh sách sách bằng async/await
-const getAllBooksAxios = async () => {
-    try {
-        const response = await axios.get('http://localhost:5000/');
-        return response.data;
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-// 2. Lấy sách theo ISBN bằng Promises (.then)
-const getBookByISBNAxios = (isbn) => {
-    return axios.get(`http://localhost:5000/isbn/${isbn}`)
-        .then(response => response.data)
-        .catch(error => console.error(error));
-};
-
-// 3. Lấy sách theo Tác giả bằng async/await
-const getBookByAuthorAxios = async (author) => {
-    try {
-        const response = await axios.get(`http://localhost:5000/author/${author}`);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-// 4. Lấy sách theo Tiêu đề bằng Promises (.then)
-const getBookByTitleAxios = (title) => {
-    return axios.get(`http://localhost:5000/title/${title}`)
-        .then(response => response.data)
-        .catch(error => console.error(error));
-};
 
 module.exports.general = public_users;
